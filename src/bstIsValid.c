1987d9a9e59f52246edd0b117433def790fc4112
Andrew <taradeevandrej15@gmail.com>
2026-04-11 22:39:36 +0300

    style(F): format code with clang-format (WebKit style)

--- src/bstIsValid.c
+++ src/bstIsValid.c
@@ -1,23 +1,24 @@
 #include "ListLogic.h"
 #include <limits.h>
 
-static bool isValidHelper(Node* node, int min, int max) {
+static bool isValidHelper(Node* node, int min, int max)
+{
     if (node == NULL) {
         return true;
     }
-    
+
     if (node->data <= min || node->data >= max) {
         return false;
     }
-    
-    return isValidHelper(node->left, min, node->data) &&
-           isValidHelper(node->right, node->data, max);
+
+    return isValidHelper(node->left, min, node->data) && isValidHelper(node->right, node->data, max);
 }
 
-bool bstIsValid(BST* tree) {
+bool bstIsValid(BST* tree)
+{
     if (tree == NULL || tree->head == NULL) {
         return true;
     }
-    
+
     return isValidHelper(tree->head, INT_MIN, INT_MAX);
 }
