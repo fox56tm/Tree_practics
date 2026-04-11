f0fc7bc6c9ef72f37b2b649a7fbe19a1109837fe
Andrew <taradeevandrej15@gmail.com>
2026-04-11 23:42:52 +0300

    fix(H): fix ListLogic.h - remove duplicates and add function prototypes

--- src/ListLogic.h
+++ src/ListLogic.h
@@ -1,47 +1,23 @@
 #ifndef LISTLOGIC_H_INCLUDED
 #define LISTLOGIC_H_INCLUDED
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef struct BstNode {
     struct BstNode* left;
     struct BstNode* right;
     int data;
-
 } Node;
 
 typedef struct BstTree {
     Node* head;
-
     int treeCount;
 } BST;
 
-void bstInsert(BST* tree, int value);
-bool bstContains(BST* tree, int value);
-void bstFree(BST* tree);
-void freeNode(Node*);
-#endif // LISTLOGIC_H_INCLUDED
-
-#ifndef LISTLOGIC_H_INCLUDED
-#define LISTLOGIC_H_INCLUDED
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct BstNode {
-    struct BstNode* left;
-    struct BstNode* right;
-    int data;
-
-} Node;
-
-typedef struct BstTree {
-    Node* head;
-
-    int treeCount;
-} BST;
-
+// Задачи A, B, C
 void bstInsert(BST* tree, int value);
 bool bstContains(BST* tree, int value);
 void bstFree(BST* tree);
@@ -50,5 +26,18 @@ void bstInorder(BST* tree);
 void nodeChangerIn(Node* node);
 void nodeDeliter(Node* node, int value);
 void bstDelete(BST* tree, int value);
+
+// Задача F
+bool bstIsValid(BST* tree);
+
+// Задача G
+int bstKthMin(BST* tree, int k);
+
+// Задача H
+typedef struct Iterator Iterator;
+Iterator* iteratorInit(BST* tree);
+bool iteratorHasNext(Iterator* it);
+int iteratorNext(Iterator* it);
+void iteratorFree(Iterator* it);
 
 #endif // LISTLOGIC_H_INCLUDED
