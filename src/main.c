c45321b351870e5e7f26429734bf32119025b403
Andrew <taradeevandrej15@gmail.com>
2026-04-11 21:20:55 +0300

    fix(H): address review comments in iterator

--- src/main.c
+++ src/main.c
@@ -2,16 +2,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    BST testTree = { NULL, 0 };
 
-    bstInsert(&testTree, 10);
-    bstInsert(&testTree, 20);
-    bstInsert(&testTree, 30);
-    bstInsert(&testTree, 40);
-    bstInsert(&testTree, 50);
-    bstInsert(&testTree, 60);
-
-    printf("%d\n", bstContains(&testTree, 100));
+int main() {
+    BST tree = {NULL, 0};
+    
+    // Вставляем узлы
+    bstInsert(&tree, 10);
+    bstInsert(&tree, 5);
+    bstInsert(&tree, 15);
+    bstInsert(&tree, 3);
+    bstInsert(&tree, 7);
+    bstInsert(&tree, 12);
+    bstInsert(&tree, 20);
+    
+    // Тест F
+    printf("Is valid BST? %d\n", bstIsValid(&tree));  // Должно быть 1
+    
+    // Тест G
+    printf("1st min: %d\n", bstKthMin(&tree, 1));  // 3
+    printf("3rd min: %d\n", bstKthMin(&tree, 3));  // 7
+    printf("7th min: %d\n", bstKthMin(&tree, 7));  // 20
+    printf("10th min (invalid): %d\n", bstKthMin(&tree, 10));  // INT_MIN
+    
+    // Тест H
+    Iterator* it = iteratorInit(&tree);
+    printf("Inorder: ");
+    while (iteratorHasNext(it)) {
+        printf("%d ", iteratorNext(it));
+    }
+    printf("\n");
+    iteratorFree(it);
+    
+    bstFree(&tree);
+    return 0;
 }
