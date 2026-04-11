c55aa04c69bd07484945cd253588ab0a3c291040
Andrew <taradeevandrej15@gmail.com>
2026-04-12 00:40:00 +0300

    fix(F): fix ListLogic.h - remove duplicate endif

--- src/ListLogic.h
+++ src/ListLogic.h
@@ -1,10 +1,10 @@
 #ifndef LISTLOGIC_H_INCLUDED
 #define LISTLOGIC_H_INCLUDED
 
-#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 typedef struct BstNode {
     struct BstNode* left;
@@ -17,33 +17,17 @@ typedef struct BstTree {
     int treeCount;
 } BST;
 
+// Задачи A, B, C
 void bstInsert(BST* tree, int value);
 bool bstContains(BST* tree, int value);
 void bstFree(BST* tree);
 void freeNode(Node*);
-
-// Задача F
-bool bstIsValid(BST* tree);
-
-// Задача G
-int bstKthMin(BST* tree, int k);
-
-// Задача H
-typedef struct Iterator Iterator;
-Iterator* iteratorInit(BST* tree);
-bool iteratorHasNext(Iterator* it);
-int iteratorNext(Iterator* it);
-/**
- * Возвращает следующий элемент в порядке inorder.
- * При вызове на исчерпанном итераторе возвращает INT_MIN.
- */
-void iteratorFree(Iterator* it);
-
-#endif
-
 void bstInorder(BST* tree);
 void nodeChangerIn(Node* node);
 void nodeDeliter(Node* node, int value);
 void bstDelete(BST* tree, int value);
+
+// Задача F
+bool bstIsValid(BST* tree);
 
 #endif // LISTLOGIC_H_INCLUDED
