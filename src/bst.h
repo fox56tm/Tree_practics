c29bebf6e722165ffcd8c190a304fc0892bd7d55
Andrew <taradeevandrej15@gmail.com>
2026-04-25 00:59:13 +0300

    fix final changes

--- src/bst.h
+++ src/bst.h
@@ -20,19 +20,3 @@ bool bstContains(BST* tree, int value);
 void bstFree(BST* tree);
 bool bstIsValid(BST* tree);
 int bstKthMin(BST* tree, int k);
-=======
-#pragma once
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct BstNode {
-    struct BstNode* left;
-    struct BstNode* right;
-    int data;
-} Node;
-
-typedef struct BstTree {
-    Node* root;
-    int nodeCount;
-} BST;
