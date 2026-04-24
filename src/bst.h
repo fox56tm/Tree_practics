551182d90cdb0c99894ae607fe3e7cc26bef49bf
Andrew <taradeevandrej15@gmail.com>
2026-04-25 00:53:28 +0300

    fix: remove merge conflict markers in bst.h

--- src/bst.h
+++ src/bst.h
@@ -19,19 +19,3 @@ void bstInsert(BST* tree, int value);
 bool bstContains(BST* tree, int value);
 void bstFree(BST* tree);
 bool bstIsValid(BST* tree);
-=======
-#pragma once
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct BstNode {
-    struct BstNode* left;
-    struct BstNode* right;
-    int data;
-} Node;
-
-typedef struct BstTree {
-    Node* root;
-    int nodeCount;
-} BST;
